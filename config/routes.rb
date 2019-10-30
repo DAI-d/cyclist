8caf5b203d4741792292f1bfef691f08b22d883e
DAI-d <mkaeeidjai@yahoo.co.jp>
2019-10-30 14:41:23 +0900

    create-japanmap
    japanmap追加
    japanmapにリンク追加
    リンク先ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'contents#index'
+  get 'contents/pref1'
+  get 'contents/pref2'
+  get 'contents/pref3'
+  get 'contents/pref4'
+  get 'contents/pref5'
   resources :users, only: [:edit, :update, :index, :show]
 end
