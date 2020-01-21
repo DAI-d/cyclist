ce1c6a93de1594fbfba6ccd664f5c2734def95a5
DAI-d <mkaeeidjai@yahoo.co.jp>
2020-01-21 23:42:27 +0900

    Update content.rb

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :prefecture
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   mount_uploader :route_image, ImageUploader
   mount_uploader :landscape_image, ImageUploader
